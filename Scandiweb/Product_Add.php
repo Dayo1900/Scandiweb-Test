c03a3c7ba88111e668e8c90429af367cd8ee747c
Dayo Abdul <73638760+Dayo1900@users.noreply.github.com>
2022-10-07 13:58:47 +0100

    Update Product_Add.php

--- Scandiweb/Product_Add.php
+++ Scandiweb/Product_Add.php
@@ -1,4 +1,4 @@
-﻿<!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
 <title>Product Add</title>
@@ -16,10 +16,13 @@
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 <link href="favicon.ico" rel="shortcut icon" type="image/x-icon"/>
+<script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
 </head>
 
 <body>
+    
 <div class="parent">
 
 <p class="class-00"> Product Add</p>
@@ -28,13 +31,13 @@
 <a class="class-0" href="Product_List.php"> 
 <span class="material-symbols-rounded" title="Next page">arrow_forward</span> 
 </a>
-<a class="class-0" href="Product_Add.php"> 
+<a class="class-0" href="Product_List.php"> 
 <span class="material-symbols-rounded" title="Back">arrow_back</span> 
 </a>
 <a class="class-0" href="Product_Add.php"> 
 <span class="material-symbols-rounded" title="Delete">close</span> 
 </a>
-<a class="class-0" href="Product_Add.php"> 
+<a class="class-0" href="Product_List.php"> 
 <span class="material-symbols-rounded" title="Delete">home</span> 
 </a>
 <input class="class-3" type="text" name="searchbar" autocomplete="ON" placeholder="Search here...">
@@ -43,19 +46,22 @@
 </button>
 </div>
 
-
-
-<div class="class-15">
-
-<div class="class-6">
-<span style="margin-left:0.5em; font-weight:bold;"> Product Add</span>
-<span class="class-11"> </span>
-<button class="class-11b" type="submit" title="Cancel product" onclick="cancel()">Cancel</button>
-<span class="class-12"></span>
-<a href="#"> <small title="Save new product" class="class-12b" onclick="save()"> Save </small></a>
+<div id="print" style="text-align:center; margin-top:1.5%; color:#006600;">
 </div>
 
-<div id="print" style="text-align:center; margin-top:3%;"></div>
+<div class="class-15">
+  <form class="pageForm" onsubmit="save(event);"> 
+    
+<div class="class-6">
+<span style="margin-left:0.5em; font-weight:bold;"> Product Add</span>
+<div class="class-19">
+<button  class="class-12b" type="submit" title="Save new product" id="submitForm"> Save</button>
+
+<a class="class-12c" href="Product_List.php" title="Cancel product">Cancel</a>
+</div>
+</div>
+
+
 
 <div id="product_form">
 <div class="class-14" id="divSKU">
@@ -75,8 +81,9 @@
 </div>
 </div>
 
+
 <div class="class-15b">
-<a id="productType" class="class-16" href="#buttonDiv"> Select Product</a>
+<input id="productType" class="class-16" onclick="cancel()" type="button" value="Select Product">
 </div>
 
 <div id="buttonDiv" class="class-16b"> 
@@ -101,33 +108,38 @@
 <div id="root">
 <div id="DVD">
 <span class="class-18d"> Size(MB)</span>
-<input type="number" title="DVD size" id="size" name="size" class="class-14b" placeholder="Enter DVD size" required>
+<input type="number" title="DVD size" id="size" name="size" class="class-14b" placeholder="Enter DVD size">
 </div>
 
 <div id="Book">
 <span class="class-18d"> Weight(kg)</span>
-<input type="number" title="Book weight" id="weight" name="weight" class="class-14b" placeholder="Enter weight" required>
+<input type="number" title="Book weight" id="weight" name="weight" class="class-14b" placeholder="Enter weight">
 </div>
 
 <div id="Furniture">
-<input type="number" style="margin-bottom:1em;" title="Height" id="height" name="height" class="class-14b" placeholder="Enter height" required>
+<input type="number" style="margin-bottom:1em;" title="Height" id="height" name="height" class="class-14b" placeholder="Enter height">
 <label class="class-18d"> Height(cm)</label>
 
-<input type="number" style="margin-bottom:1em;" title="Length" id="length" name="length" class="class-14b" placeholder="Enter length" required>
+<input type="number" style="margin-bottom:1em;" title="Length" id="length" name="length" class="class-14b" placeholder="Enter length">
 <label class="class-18d"> Length(cm)</label>
 
-<input type="number" style="margin-bottom:1em;" title="Width" id="width" name="width" class="class-14b" placeholder="Enter width" required>
+<input type="number" style="margin-bottom:1em;" title="Width" id="width" name="width" class="class-14b" placeholder="Enter width">
 <label class="class-18d"> Width(cm)</label>
 </div>
 </div>
-
+</form>
 </div>
+
+
+
 <p class="class-13"> Scandiweb Test assignment</p>
 
 
-<img class="class-10b" src="src/images/more_horiz.png">
+<span class="material-symbols-rounded class-10b">
+more_horiz
+</span>
 </div>
 </body>
-<script type="text/javascript" src="src/JS/deleteInput.js" async></script>
-<script type="text/javascript" src="src/JS/saveInput.js" async></script>
+<script type="text/javascript" src="src/JS/emptyAll_inputValue.js" async></script>
+<script type="text/javascript" src="src/JS/saveInput_jquery.js" async></script>
 </html>
