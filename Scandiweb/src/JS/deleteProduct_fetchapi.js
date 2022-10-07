3b8d56cb3ff218c5a6eeb0f491cf8c49a4527722
Dayo Abdul <73638760+Dayo1900@users.noreply.github.com>
2022-10-07 18:45:47 +0100

    Create deleteProduct_fetchapi.js

---
+++ Scandiweb/src/JS/deleteProduct_fetchapi.js
@@ -0,0 +1,25 @@
+
+//mass delete products on Product_list page
+function deleting() {
+ 
+  let Root = document.querySelectorAll('.class-9b');
+  let speak = document.querySelector("#print");
+  let skuArray = [];
+  let deleteSKU = new FormData();
+  
+  for(var i=0; i<Root.length; i++) {
+    var input = Root[i].querySelector("#delete-checkbox");
+   
+    if(input.checked) {
+    var Value = input.value;
+    skuArray.push(Value);
+    Root[i].style.display = "none";	
+    }
+  }
+  
+  deleteSKU.append("box", skuArray);
+  
+fetch("https://plutobakery.000webhostapp.com/Scandiweb/env/massDelete.php", {method: "POST", body: deleteSKU })
+  .then(res => res.text())
+  .then((results) => {speak.innerHTML = results;});
+}
